6122f14d672c860ae31b1ea5592514a39c8d1f6a
fazal <fazal.l@orangewebtech.com>
2024-12-07 14:20:37 +0530

    - Latest Code Push Main Branch

--- lib/screen/auth/login/login_view.dart
+++ lib/screen/auth/login/login_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_state_manager/src/simple/get_view.dart';
 import 'package:ultimatix_hrms_flutter/app/app_colors.dart';
 import 'package:ultimatix_hrms_flutter/app/app_font_weight.dart';
 import 'package:ultimatix_hrms_flutter/app/app_images.dart';

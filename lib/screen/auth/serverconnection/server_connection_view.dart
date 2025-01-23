6f842d5b26417e792e70455d35d690a523a4ea63
sahil.g <flutter@orangewebtech.com>
2025-01-23 12:32:45 +0530

    merged (23-01-2025 12:32PM)

--- lib/screen/auth/serverconnection/server_connection_view.dart
+++ lib/screen/auth/serverconnection/server_connection_view.dart
@@ -24,77 +24,83 @@ class ServerConnectionView extends GetView<ServerConnectionController> {
         backgroundColor: Colors.white,
         body: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: Column(
-            //mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                child: Padding(
-                  padding: EdgeInsets.only(
-                      top: MediaQuery.of(context).size.width / 4),
-                  child: const CommonAppImage(
-                    imagePath: AppImages.icserverlogo,
-                    height: 200,
-                    width: 200,
-                    fit: BoxFit.fill,
+          child: SingleChildScrollView(
+            child: Column(
+              //mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                        top: MediaQuery.of(context).size.width / 4),
+                    child: const CommonAppImage(
+                      imagePath: AppImages.icserverlogo,
+                      height: 200,
+                      width: 200,
+                      fit: BoxFit.fill,
+                    ),
                   ),
                 ),
-              ),
-              CommonText(
-                text: 'Please enter server name',
-                color: AppColors.color2F2F31,
-                fontSize: 16,
-                fontWeight: AppFontWeight.w400,
-              ).paddingOnly(top: 40),
-              CommonAppInputNew(
-                hintStyle: AppFonts.interTextStyle()
-                    .copyWith(color: AppColors.color7B758E),
-                textEditingController: controller.serverController,
-                focusNode: controller.serverFocus,
-                hintText: 'Enter Server Name',
-                // prifixPadding: const EdgeInsets.all(12),
-                hintColor: AppColors.colorBlack.withOpacity(0.3),
-              ).paddingOnly(top: 15),
-              CommonButtonNew(
-                  buttonText: 'Connect', onPressed: () {}, isLoading: false).paddingOnly(top: 20),
-              // SizedBox(
-              //   width: MediaQuery.of(context).size.width,
-              //   height: MediaQuery.of(context).size.width / 8,
-              //   child: ElevatedButton(
-              //     style: ButtonStyle(
-              //         backgroundColor: WidgetStateColor.resolveWith(
-              //           (states) => AppColors.colorPurple,
-              //         ),
-              //         shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-              //             RoundedRectangleBorder(
-              //           borderRadius: BorderRadius.circular(10.0),
-              //         ))),
-              //     onPressed: () {
-              //       controller.serverConnectionDialog(context);
-              //       //Get.toNamed(AppRoutes.dashBoard);
-              //     },
-              //     child: CommonText(
-              //       text: AppString.connect,
-              //       color: AppColors.colorWhite,
-              //       fontSize: 16,
-              //       fontWeight: AppFontWeight.w400,
-              //     ),
-              //   ),
-              // ).paddingOnly(left: 30, right: 30, top: 20),
-              // GestureDetector(
-              //   onTap: () {
-              //     Get.toNamed(AppRoutes.loginRoute);
-              //   },
-              //   child: CommonText(
-              //     padding:
-              //         const EdgeInsets.only(top: 20, bottom: 12, left: 12),
-              //     text: AppString.cancel,
-              //     color: AppColors.colorPurple,
-              //     fontSize: 18,
-              //     fontWeight: AppFontWeight.medium,
-              //   ),
-              // )
-            ],
+                CommonText(
+                  text: 'Please enter server name',
+                  color: AppColors.color2F2F31,
+                  fontSize: 16,
+                  fontWeight: AppFontWeight.w400,
+                ).paddingOnly(top: 40),
+                CommonAppInputNew(
+                  hintStyle: AppFonts.interTextStyle()
+                      .copyWith(color: AppColors.color7B758E),
+                  textEditingController: controller.serverController,
+                  focusNode: controller.serverFocus,
+                  hintText: 'Enter Server Name',
+                  textInputAction: TextInputAction.done,
+                  // prifixPadding: const EdgeInsets.all(12),
+                  hintColor: AppColors.colorBlack.withOpacity(0.3),
+                ).paddingOnly(top: 15),
+                CommonButtonNew(
+                        buttonText: 'Connect',
+                        onPressed: () {},
+                        isLoading: false)
+                    .paddingOnly(top: 20),
+                // SizedBox(
+                //   width: MediaQuery.of(context).size.width,
+                //   height: MediaQuery.of(context).size.width / 8,
+                //   child: ElevatedButton(
+                //     style: ButtonStyle(
+                //         backgroundColor: WidgetStateColor.resolveWith(
+                //           (states) => AppColors.colorPurple,
+                //         ),
+                //         shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                //             RoundedRectangleBorder(
+                //           borderRadius: BorderRadius.circular(10.0),
+                //         ))),
+                //     onPressed: () {
+                //       controller.serverConnectionDialog(context);
+                //       //Get.toNamed(AppRoutes.dashBoard);
+                //     },
+                //     child: CommonText(
+                //       text: AppString.connect,
+                //       color: AppColors.colorWhite,
+                //       fontSize: 16,
+                //       fontWeight: AppFontWeight.w400,
+                //     ),
+                //   ),
+                // ).paddingOnly(left: 30, right: 30, top: 20),
+                // GestureDetector(
+                //   onTap: () {
+                //     Get.toNamed(AppRoutes.loginRoute);
+                //   },
+                //   child: CommonText(
+                //     padding:
+                //         const EdgeInsets.only(top: 20, bottom: 12, left: 12),
+                //     text: AppString.cancel,
+                //     color: AppColors.colorPurple,
+                //     fontSize: 18,
+                //     fontWeight: AppFontWeight.medium,
+                //   ),
+                // )
+              ],
+            ),
           ),
         ),
       ),
